193027251138f568f33034539c407ec962be95a0
Alessioplt <alessio.pulit@gmail.com>
2022-12-11 13:38:35 +0100

    visualise genome with layer order

--- main.py
+++ main.py
@@ -3,30 +3,27 @@ import random
 from NEAT import Gene_manager
 from NEAT import Genome
 from visualise_genome import Graph
-import graph
-def fonction(hh):
-    print(hh)
+from NEAT import all_functions
 
 
 new_gene_manager = Gene_manager.GeneManager()
-new_gene_manager.add_gene("A", "sensor", fonction)
-new_gene_manager.add_gene("B", "sensor", fonction)
-new_gene_manager.add_gene("C", "sensor", fonction)
-new_gene_manager.add_gene("1", "output", fonction)
-new_gene_manager.add_gene("2", "output", fonction)
-new_gene_manager.add_gene("3", "output", fonction)
-new_gene_manager.add_gene("stfu", "hidden", fonction)
+new_gene_manager.add_gene("A", "sensor", all_functions.fonction)
+new_gene_manager.add_gene("B", "sensor", all_functions.fonction)
+new_gene_manager.add_gene("C", "sensor", all_functions.fonction)
+new_gene_manager.add_gene("1", "output", all_functions.fonction)
+new_gene_manager.add_gene("2", "output", all_functions.fonction)
+new_gene_manager.add_gene("3", "output", all_functions.fonction)
+new_gene_manager.add_gene("stfu", "hidden", all_functions.fonction)
 
 
 new_genome = Genome.Genome(new_gene_manager)
 new_genome.create(22)
-new_genome.show_connections()
-graph_v1 = Graph()
-graph_v1.create_graph(new_genome.connections)
-graph_v2 = graph.Graph()
-graph_v2.create_graph(new_genome.connections,   len(new_gene_manager.get_all_gene_type("sensor")),
-                                                len(new_gene_manager.get_all_gene_type("output")),
-                                                len(new_gene_manager.get_all_gene_type("hidden")))
+#new_genome.show_connections()
+
+graph = Graph()
+graph.create_graph(new_genome.connections,  new_gene_manager.get_all_gene_type("sensor"),
+                                            new_gene_manager.get_all_gene_type("output"),
+                                            new_gene_manager.get_all_gene_type("hidden"))
 print("new node")
-new_genome.mutate(10000, 1)
-new_genome.show_connections()
+#new_genome.mutate(10000, 1)
+#new_genome.show_connections()
--- visualise_genome.py
+++ visualise_genome.py
@@ -1,34 +1,73 @@
-import matplotlib.pyplot as pelote
-import networkx
+import itertools
+import matplotlib.pyplot as plt
 import networkx as nx
-
-
+"""            "violet",
+            "violet",
+            "violet",
+            "limegreen",
+            "darkorange",
+        """
 class Graph:
-    def create_graph(self, connections):
+    def create_graph(self, connections, number_input, number_output, number_hidden):
+        subset_sizes = [len(number_input), len(number_hidden), len(number_output)]
+        subset_color = [
+            "gold",
+            "violet",
+            "limegreen",
+        ]
         G = nx.Graph()
+        pos = {}
+        layer_in = -1
+        layer_hidden = -1
+        layer_out = -1
         for connection in connections.keys():
             self.add_edge(G, connection.node_in.name, connection.node_out.name, (float("%.4f" % connections[connection][0])))
+            if connection.node_in.name not in pos:
+                if connection.node_in in number_input:
+                    pos[connection.node_in.name] = (0, layer_in)
+                    if layer_in > 0:
+                        layer_in +=1
+                    layer_in = layer_in * -1
+                else:
+                    pos[connection.node_in.name] = (1, layer_hidden)
+                    if layer_hidden > 0:
+                        layer_hidden +=1
+                    layer_hidden = layer_hidden * -1
+
+            if connection.node_out.name not in pos:
+                if connection.node_out in number_output:
+                    pos[connection.node_out.name] = (2, layer_out)
+                    if layer_out > 0:
+                        layer_out +=1
+                    layer_out = layer_out * -1
+                else:
+                    pos[connection.node_out.name] = (1, layer_hidden)
+                    if layer_hidden > 0:
+                        layer_hidden +=1
+                    layer_hidden = layer_hidden * -1
+        print(pos)
         elarge_pos = [(u, v) for (u, v, d) in G.edges(data=True) if d["weight"] > 1]
         esmall_pos = [(u, v) for (u, v, d) in G.edges(data=True) if 0 < d["weight"] <= 1]
         elarge_neg = [(u, v) for (u, v, d) in G.edges(data=True) if -1 < d["weight"] <= 0]
         esmall_neg = [(u, v) for (u, v, d) in G.edges(data=True) if d["weight"] <= -1]
-        pos = nx.spring_layout(G, seed=1)  # positions for all nodes - seed for reproducibility
+        print(elarge_pos, esmall_pos)
         nx.draw_networkx_nodes(G, pos, node_size=700)
         nx.draw_networkx_edges(G, pos, edgelist=elarge_pos, width=6, edge_color="b")
-        nx.draw_networkx_edges(G, pos, edgelist=esmall_pos, width=6, edge_color="b")
-        nx.draw_networkx_edges(G, pos, edgelist=elarge_neg, width=3, edge_color="r")
+        nx.draw_networkx_edges(G, pos, edgelist=esmall_pos, width=3, edge_color="b")
+        nx.draw_networkx_edges(G, pos, edgelist=elarge_neg, width=6, edge_color="r")
         nx.draw_networkx_edges(G, pos, edgelist=esmall_neg, width=3, edge_color="r")
         # node labels
         nx.draw_networkx_labels(G, pos, font_size=20, font_family="sans-serif")
         # edge weight labels
         edge_labels = nx.get_edge_attributes(G, "weight")
-        nx.draw_networkx_edge_labels(G, pos, edge_labels)
+        nx.draw_networkx_edge_labels(G, pos, edge_labels, label_pos=0.69)
+        nx.set_node_attributes(G, pos, 'coord')
 
-        ax = pelote.gca()
+        ax = plt.gca()
         ax.margins(0.08)
-        pelote.axis("off")
-        pelote.tight_layout()
-        pelote.show()
+        plt.axis("off")
+        plt.tight_layout()
+        plt.show()
 
     def add_edge(self, graph, input, output, weight):
         graph.add_edge(u_of_edge=input, v_of_edge=output, weight=weight)
