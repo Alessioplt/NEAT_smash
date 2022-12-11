fd2549fd97bb070bce3636a94ece9ac49744559a
Alessioplt <alessio.pulit@gmail.com>
2022-12-11 15:33:30 +0100

    better genome visualisation

--- visualise_genome.py
+++ visualise_genome.py
@@ -17,47 +17,45 @@ class Graph:
         ]
         G = nx.Graph()
         pos = {}
-        layer_in = -1
-        layer_hidden = -1
-        layer_out = -1
+        layer_in = 0
+        layer_hidden = -0.5
+        layer_out = 0
         for connection in connections.keys():
             self.add_edge(G, connection.node_in.name, connection.node_out.name, (float("%.4f" % connections[connection][0])))
             if connection.node_in.name not in pos:
                 if connection.node_in in number_input:
+
                     pos[connection.node_in.name] = (0, layer_in)
-                    if layer_in > 0:
-                        layer_in +=1
+                    if layer_in >= 0:
+                        layer_in += 3
                     layer_in = layer_in * -1
                 else:
                     pos[connection.node_in.name] = (1, layer_hidden)
-                    if layer_hidden > 0:
-                        layer_hidden +=1
+                    if layer_hidden >= 0:
+                        layer_hidden += 1
                     layer_hidden = layer_hidden * -1
-
             if connection.node_out.name not in pos:
                 if connection.node_out in number_output:
                     pos[connection.node_out.name] = (2, layer_out)
-                    if layer_out > 0:
-                        layer_out +=1
+                    if layer_out >= 0:
+                        layer_out += 3
                     layer_out = layer_out * -1
                 else:
                     pos[connection.node_out.name] = (1, layer_hidden)
-                    if layer_hidden > 0:
-                        layer_hidden +=1
+                    if layer_hidden >= 0:
+                        layer_hidden += 1
                     layer_hidden = layer_hidden * -1
-        print(pos)
         elarge_pos = [(u, v) for (u, v, d) in G.edges(data=True) if d["weight"] > 1]
         esmall_pos = [(u, v) for (u, v, d) in G.edges(data=True) if 0 < d["weight"] <= 1]
         elarge_neg = [(u, v) for (u, v, d) in G.edges(data=True) if -1 < d["weight"] <= 0]
         esmall_neg = [(u, v) for (u, v, d) in G.edges(data=True) if d["weight"] <= -1]
-        print(elarge_pos, esmall_pos)
         nx.draw_networkx_nodes(G, pos, node_size=700)
         nx.draw_networkx_edges(G, pos, edgelist=elarge_pos, width=6, edge_color="b")
         nx.draw_networkx_edges(G, pos, edgelist=esmall_pos, width=3, edge_color="b")
         nx.draw_networkx_edges(G, pos, edgelist=elarge_neg, width=6, edge_color="r")
         nx.draw_networkx_edges(G, pos, edgelist=esmall_neg, width=3, edge_color="r")
         # node labels
-        nx.draw_networkx_labels(G, pos, font_size=20, font_family="sans-serif")
+        nx.draw_networkx_labels(G, pos, font_size=10, font_family="sans-serif")
         # edge weight labels
         edge_labels = nx.get_edge_attributes(G, "weight")
         nx.draw_networkx_edge_labels(G, pos, edge_labels, label_pos=0.69)
